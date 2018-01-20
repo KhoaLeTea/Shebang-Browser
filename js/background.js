4cb3a65ac8dc6a82eb0195bc125d38d5e0cfd389
LUCIAHUO <luciahuo@sas.upenn.edu>
2018-01-20 12:16:05 -0500

    Alex needs to talk - plz do talk

--- js/background.js
+++ js/background.js
@@ -1,37 +1,34 @@
 console.log("Hello from the #!Browser");
 
-
-chrome.omnibox.setDefaultSuggestion({
-  description: 'A FUCKING SUGGESTION'
-});
-
-chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
-  console.log('hello')
-  if (text.search('coffee') > -1) {
-        var suggestions = [];
-
-        suggestions.push({ content: 'Coffee - Wikipedia', description: 'Coffee - Wikipedia' });
-        suggestions.push({ content: 'Starbucks Coffee', description: 'Starbucks Coffee' });
-        suggest(suggestions);
-    }
-});
-
 chrome.omnibox.onInputEntered.addListener(function(text) {
-
   chrome.tabs.query({
       active: true,
       currentWindow: true
   }, function(tabs) {
       chrome.tabs.sendMessage(
-        552,
+        tabs[0].id,
         {
           show: true,
           text: text
         },
-        function() {
-          console.log("Message from the content script:");
-          console.log(response.response);
+        function(response) {
+          console.log("received something");
+          alert(response.response);
         }).catch(onError);
   });
 
+});
+
+// store the history
+var history = [];
+var historySize = 0;
+
+chrome.omnibox.onInputEntered.addListener(function (text, suggest) {
+  console.log(history)
+  history.append(text);
+  historySize+=1;
+  // the universal storage for all terminal inputs and outputs
+  chrome.storage.sync.set({'history':
+    history
+  });
 });

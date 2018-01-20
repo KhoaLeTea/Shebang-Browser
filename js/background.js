2baff3d59e3d2da7e8ebcbb6e8731858b73338fa
LUCIAHUO <luciahuo@sas.upenn.edu>
2018-01-20 15:06:56 -0500

    deleted double

--- js/background.js
+++ js/background.js
@@ -30,17 +30,3 @@ chrome.omnibox.onInputEntered.addListener(function (text, suggest) {
     history
   });
 });
-
-// store the history
-var history = [];
-var historySize = 0;
-
-chrome.omnibox.onInputEntered.addListener(function (text, suggest) {
-  console.log(history)
-  history.append(text);
-  historySize+=1;
-  // the universal storage for all terminal inputs and outputs
-  chrome.storage.sync.set({'history':
-    history
-  });
-});

7a27a462053c4d0f5ba0e40124b77e0ca7db87f7
Alex Goodkind <alex@galex.me>
2018-01-20 15:31:05 -0500

    fixed show response

--- js/background.js
+++ js/background.js
@@ -1,5 +1,9 @@
 console.log("Hello from the #!Browser");
 
+// store the history
+var arr = [];
+var historySize = 0;
+
 chrome.omnibox.onInputEntered.addListener(function(text) {
   chrome.tabs.query({
       active: true,
@@ -12,35 +16,16 @@ chrome.omnibox.onInputEntered.addListener(function(text) {
           text: text
         },
         function(response) {
-          console.log("message recieve status: " + response.response);
+          console.log("message recieve status: " + response.status);
         });
   });
 
-});
-
-
-var history = [];
-var historySize = 0;
-
-chrome.omnibox.onInputEntered.addListener(function (text, suggest) {
-  history.append(text);
+  console.log(arr)
+  arr.push(text);
   historySize+=1;
   // the universal storage for all terminal inputs and outputs
   chrome.storage.sync.set({'history':
-    history
+    arr
   });
-});
 
-// store the history
-var history = [];
-var historySize = 0;
-
-chrome.omnibox.onInputEntered.addListener(function (text, suggest) {
-  console.log(history)
-  history.append(text);
-  historySize+=1;
-  // the universal storage for all terminal inputs and outputs
-  chrome.storage.sync.set({'history':
-    history
-  });
 });

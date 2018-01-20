933b26dd29e25a75b9e2483126ba38146e5047b4
Alex Goodkind <alex@galex.me>
2018-01-20 16:51:49 -0500

    updated for lucia

--- js/background.js
+++ js/background.js
@@ -32,5 +32,6 @@ chrome.runtime.onMessage.addListener(function(message) {
   console.log("message recieved: " + message.tabOpened);
   if (message.tabOpened) {
     chrome.tabs.create({"url": "terminal_tab.html"});
+    // possible: send a message to the new tab with the history
   }
 });

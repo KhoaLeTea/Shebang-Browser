3b63563f2129cfdf6dad0802455e9212252a9f8e
Alex Goodkind <alex@galex.me>
2018-01-21 01:01:02 -0500

    moved list of commands to cleanup code

--- js/bash.js
+++ js/bash.js
@@ -207,3 +207,72 @@ var Bash = function(selector, options) {
   };
   self.initialise();
 };
+
+// list of commands
+var commands = {
+  grunt: function(bash, next) {
+    bash.post(
+      'Running "jshint:gruntfile" (jshint) task',
+      0,
+      false,
+      true,
+      bash.post(
+        '>> 1 file lint free.',
+        500,
+        false,
+        true,
+        bash.post(
+          '&nbsp;',
+          600,
+          false,
+          true,
+          bash.post(
+            'Running "uglify:dist" (uglify) task',
+            700,
+            false,
+            true,
+            bash.post(
+              'Running "uglify:dist" (uglify) task',
+              700,
+              false,
+              true,
+              bash.post(
+                'File "dist/scripts.min.js" created.',
+                1600,
+                false,
+                true,
+                bash.post(
+                  'Uncompressed size: 389 bytes.',
+                  1800,
+                  false,
+                  true,
+                  bash.post(
+                    '&nbsp;',
+                    1900,
+                    false,
+                    true,
+                    bash.post(
+                      'Done, without errors.',
+                      2000,
+                      false,
+                      true,
+                      function() {
+                        return next();
+                      }
+                    )
+                  )
+                )
+              )
+            )
+          )
+        )
+      )
+    );
+  },
+  ls: function(bash, next) {
+    bash.post('Look at all of these directories', 0, false, true, function() {
+      return next();
+    });
+  }
+};
+
